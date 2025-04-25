a0ad1942906ede6c9ee20b9a9fb41ca8c93278ce
divineion <najima.guermoudi@gmail.com>
2025-04-25 15:28:09 +0200

    Refactor RootLayout.tsx
    - use and simplify the default Next.js RootLayout component structure
    - import global styles via index.scss,
    - import Bootstrap CSS,
    - set the <html> to "fr".

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import "../styles/index.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
