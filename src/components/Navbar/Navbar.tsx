83bba00ae416d36632b0db8884ae2f704ec6925f
divineion <najima.guermoudi@gmail.com>
2025-06-02 20:26:47 +0200

    feat(navbar): add logout button with AuthContext integration
    - replace logout Link with a button to trigger logout action,
    - use useAuth hook to access logout function and handle user logout,
    - add 'app-name' class to app title link for styling.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,27 @@
+"use client"; // useRouter fonctionne côté client
 import Link from "next/link";
+import {useAuth} from "@/contexts/AuthContext";
 
 const Navbar = () => {
+
+    const { logout } = useAuth();
+    const handleLogout = () => {
+        logout();
+    }
+
     return (
         <>
             <nav className={"header-nav"}>
-                <Link href={"/"}>Pay My Buddy</Link>
+                <Link className={"app-name"} href={"/"}>Pay My Buddy</Link>
                 <ul>
                     <li><Link href={"/transfer"}>Transférer</Link></li>
                     <li><Link href={"/profile"}>Profil</Link></li>
                     <li><Link href={"/add-relation"}>Ajouter relation</Link></li>
-                    <li><Link href={"/logout"}>Se déconnecter</Link></li>
+                    <li>
+                        <button className={"header-nav__logout-button"} onClick={handleLogout}>
+                            Se déconnecter
+                        </button>
+                    </li>
                 </ul>
             </nav>
         </>
