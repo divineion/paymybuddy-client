d4990b5458066267d865164fc1081f0e30b9ff38
divineion <najima.guermoudi@gmail.com>
2025-06-05 09:57:16 +0200

    feat(TransferPage): add Transfer component and metadata
    Display the Transfer form and set the page title using metadata.

--- src/app/transfer/page.tsx
+++ src/app/transfer/page.tsx
@@ -1,10 +1,16 @@
 import Header from "@/components/Header/Header";
+import Transfer from "@/components/Transfer/Transfer";
+import {TRANSFER_PAGE_TITLE} from "@/constants/titleTags";
 
+export const metadata = {
+    title: TRANSFER_PAGE_TITLE,
+};
 const TransferPage = () => {
     return (
         <>
             <Header></Header>
             <main className={"main"}>
+                <Transfer/>
             </main>
         </>
     )
