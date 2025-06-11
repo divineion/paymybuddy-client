cea5cb8841b2df9b731aa4dcefd69aef64a89c65
divineion <najima.guermoudi@gmail.com>
2025-06-11 03:22:18 +0200

    feat(auth-error): redirect if token is invalid

--- src/components/Profile/ProfileComponent.tsx
+++ src/components/Profile/ProfileComponent.tsx
@@ -1,10 +1,25 @@
+"use client";
 import ProfileForm from "../Forms/ProfileForm"
+import {useEffect} from "react";
+import {frontLoginRoute} from "@/constants/frontRoutes";
+import {useAuth} from "@/contexts/AuthContext";
+import {useRouter} from "next/navigation";
 
 const ProfileComponent = () => {
+    const {loading, isLoggedIn, authCheck} = useAuth();
+    const router = useRouter();
+    useEffect(() => {
+        authCheck();
+    }, []);
+
+    useEffect(() => {
+        if (!loading && !isLoggedIn) {
+            router.push(frontLoginRoute);
+        }
+    }, [loading, isLoggedIn]);
     return (
         <>
         <ProfileForm></ProfileForm>
-
         </>
 
     )
